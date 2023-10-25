e2fa70c0bd5ca797f8295537757ea3c29dc2b575
Konstantin-Filipov <kosio1234545@gmail.com>
2023-10-25 20:31:19 +0200

    move retrofit instance from MainActivity.kt

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.myapplication
 
-import android.content.ContentValues
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -52,14 +51,10 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.myapplication.data.Service
+import com.example.myapplication.network.getServices
 import com.example.myapplication.ui.theme.MyApplicationTheme
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
-const val BASE_URL = "https://serviceregistry.navelink.org/api/"
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -135,40 +130,6 @@ fun SimpleOutlinedTextFieldSample() {
         value = text,
         onValueChange = { text = it },
     )
-}
-
-fun getServices(callback: (List<Service>?) -> Unit){
-    val api = Retrofit.Builder()
-        .baseUrl(BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
-        .create(API::class.java)
-
-    api.getCompanies().enqueue(object : Callback<List<Service>> {
-        override fun onResponse(
-            call: Call<List<Service>>,
-            response: Response<List<Service>>
-        ) {
-            if (response.isSuccessful) {
-                val serviceList = response.body() // save data in list
-
-                if (serviceList != null) {
-                    for (service in serviceList) {
-                        Log.i(ContentValues.TAG, "onResponse: ID: ${service.id}")
-                        Log.i(ContentValues.TAG, "onResponse: Name: ${service.name}")
-                        Log.i(ContentValues.TAG, "onResponse: Keywords: ${service.keywords}")
-                        Log.i(ContentValues.TAG, "onResponse: Geometry Type: ${service.geometry}")
-                    }
-                }
-                callback(serviceList)
-            }
-        }
-
-        override fun onFailure(call: Call<List<Service>>, t: Throwable) {
-            Log.e(ContentValues.TAG, "Request failed: ${t.message}", t)
-            callback(null)
-        }
-    })
 }
 
 @Composable
